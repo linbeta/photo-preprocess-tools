cb8ff1ae2b415ceb99b4ec979f465c2128ae2078
Beta Lin <sb2828sb@gmail.com>
2021-12-21 11:59:07 +0800

    resize main add try/except

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ def mkdir(path):
 
 
 # 用ITEM來存哪個食材資料夾及檔名
-ITEM = "val_img"
+ITEM = "52_cucumber"
 # 設定圖片讀取路徑，該路徑下包含jpg格式的照片
-DATADIR = f"C:/Users/sb282/Downloads/" + ITEM
+DATADIR = f"D:\OneDrive\Learning\AI Class_TibaMe02\Team Project\original_files\\" + ITEM
 # 設定存檔路徑
 SAVE_DIR = "D:\OneDrive\Learning\AI Class_TibaMe02\Team Project\\000_resize_224_square\\" + ITEM
 # 設定目標畫素大小，專案需一致，為32的倍數
@@ -54,23 +54,28 @@ for i in img_list:
     #呼叫cv2.imread讀入圖片，讀入格式為IMREAD_COLOR
     img_array = cv2.imread(os.path.join(path, i), cv2.IMREAD_COLOR)
     # print(img_array)
-    y, x = img_array.shape[:2]
-    y = int(y)
-    x = int(x)
-    # print(y//2, x//2)
-    if y > x:
-        img_array2 = img_array[(y//2 - x//2):(y//2 + x//2), :img_array.shape[1]]
-        # cv2.imshow("1", img_array2)
-    else:
-        img_array2 = img_array[:img_array.shape[0], (x//2 - y//2):(x//2 + y//2)]
-        # cv2.imshow("2", img_array2)
-    '''呼叫cv2.resize函式resize圖片'''
-    new_array = cv2.resize(img_array2, (IMG_SIZE, IMG_SIZE))
-    # cv2.imshow('3', new_array)
-    img_name = "_" + str(ind)+'.jpg'
-    # 存檔路徑
+    try:
+        y, x = img_array.shape[:2]
+        y = int(y)
+        x = int(x)
+        # print(y//2, x//2)
+        if y > x:
+            img_array2 = img_array[(y//2 - x//2):(y//2 + x//2), :img_array.shape[1]]
+            # cv2.imshow("1", img_array2)
+        else:
+            img_array2 = img_array[:img_array.shape[0], (x//2 - y//2):(x//2 + y//2)]
+            # cv2.imshow("2", img_array2)
 
-    save_path = SAVE_DIR + "\\" + ITEM + img_name
+        '''呼叫cv2.resize函式resize圖片'''
+        new_array = cv2.resize(img_array2, (IMG_SIZE, IMG_SIZE))
+        # cv2.imshow('3', new_array)
+        img_name = "_" + str(ind)+'.jpg'
+        # 存檔路徑
+
+        save_path = SAVE_DIR + "\\" + ITEM + img_name
+    except:
+        pass
+
     print(save_path)
     '''呼叫cv.2的imwrite函式儲存圖片'''
     # print(i)
